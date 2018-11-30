6c1e3f150997cb1b512216f35ac16ff6d35c5891
Yong5903 <45242754+Yong5903@users.noreply.github.com>
2018-11-29 16:26:09 -0800

    Update account.js

--- account.js
+++ account.js
@@ -1,71 +1,75 @@
-console.log('connected to account.js');
-const fs = require('fs');
-const sign = require('./sign')
+console.log("connected to account.js");
+const fs = require("fs");
+const sign = require("./sign");
 
-var database = fs.readFileSync('database.json');
+var database = fs.readFileSync("database.json");
 var existedAcc = JSON.parse(database);
-var account = {};
-
 
 module.exports.newAccount = (account, password, birthday) => {
-
-    userSign = sign.getSign(birthday);
+    var userSign = sign.getSign(birthday);
     var newAccount = {
         "username": account,
         "password": password,
-        "sign": userSign
+        "sign": userSign,
+        "token": Math.random().toString(16).slice(2, 16)
     };
 
     var isExist = false;
-
-    for (i = 0; i < existedAcc.length; i++) {
+    for (var i = 0; i < existedAcc.length; i++) {
         if (existedAcc[i].username === account) {
-            console.log('Account name exists');
+            console.log("Account name exists");
             isExist = true;
-            break
+            break;
         }
     }
     if (isExist === false) {
         existedAcc.push(newAccount);
         var result = JSON.stringify(existedAcc);
-        fs.writeFileSync('database.json', result);
-        console.log('\nNew User added into system!\n');
-        console.log('Username: ' + newAccount.username);
-        console.log('Password: ' + newAccount.password);
-        console.log('Sign: ' + newAccount.sign);
+        fs.writeFileSync("database.json", result);
+        console.log("\nNew User added into system!\n");
+        console.log(`Username: ${newAccount.username}`);
+        console.log(`Password: ${newAccount.password}`);
+        console.log(`Sign: ${newAccount.sign}`);
+        return newAccount.token;
     }
+    return false;
 };
 
 module.exports.Login = (username, password) => {
-
-    var accountValid = false;
-    var passwordValid = false;
-    console.log(existedAcc)
-    for (i = 0; i < existedAcc.length; i++) {
+    console.log(existedAcc);
+    for (var i = 0; i < existedAcc.length; i++) {
         if (existedAcc[i].username === username) {
-            var accountValid = true;
-            if (existedAcc[i].password === password) {
-                var passwordValid = true;
+            if (existedAcc[i].password === password)
+            {
+                return existedAcc[i].token;
             } else {
                 console.log("wrong password");
             }
         }
-
     }
-
+    return false;
 };
 
-module.exports.getSign = (username) => {
-
-    for (i = 0; i < existedAcc.length; i++) {
-
-        if (existedAcc[i].username === username) {
+module.exports.getSign = (username) =>
+{
+    for (var i = 0; i < existedAcc.length; i++)
+    {
+        if (existedAcc[i].username === username)
+        {
             return existedAcc[i].sign;
-            console.log(existedAcc[i].sign);
         }
     }
-
+    return false;
 };
 
-
-
+module.exports.getSignByToken = (token) =>
+{
+    for (var i = 0; i < existedAcc.length; i++)
+    {
+        if (existedAcc[i].token === token)
+        {
+            return existedAcc[i].sign;
+        }
+    }
+    return false;
+};
